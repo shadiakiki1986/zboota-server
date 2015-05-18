7904abe8b48db0fc739425bab9f3b417b1dd761a
Shadi Akiki <shadiakiki1986@gmail.com>
2015-05-18 20:16:43 +0000

    added mailgun variables to travis env vars

--- scripts/env2conf.sh
+++ scripts/env2conf.sh
@@ -8,6 +8,10 @@
 if [ -z "$ZBOOTA_SERVER_AWS_KEY" ]; then echo "Please set AWS_KEY env var"; exit -1; fi
 if [ -z "$ZBOOTA_SERVER_AWS_ACCESS" ]; then echo "Please set ACCESS env vars 1st"; exit -1; fi
 if [ -z "$ZBOOTA_SERVER_AWS_REGION" ]; then echo "Please set Region env vars 1st"; exit -1; fi
+if [ -z "$MAILGUN_KEY" ]; then echo "Please set env vars. Check scripts/env2conf.sh"; exit -1; fi
+if [ -z "$MAILGUN_DOMAIN" ]; then echo "Please set env vars. Check scripts/env2conf.sh"; exit -1; fi
+if [ -z "$MAILGUN_FROM" ]; then echo "Please set env vars. Check scripts/env2conf.sh"; exit -1; fi
+if [ -z "$MAILGUN_PUBLIC_KEY" ]; then echo "Please set env vars. Check scripts/env2conf.sh"; exit -1; fi
 
 echo "<?php"
 echo "define('ROOT','/home/travis/build/shadiakiki1986/zboota-server');"
@@ -19,4 +23,8 @@ echo "define('S3_BUCKET','zboota-server');"
 echo "define('S3_FOLDER','photos');"
 echo "define('MAX_PASS_FAIL',3);"
 echo "define('MY_CURL_TIMEOUT',10);"
+echo "define('MAILGUN_KEY','$MAILGUN_KEY');"
+echo "define('MAILGUN_DOMAIN','$MAILGUN_DOMAIN');"
+echo "define('MAILGUN_FROM','$MAILGUN_FROM');"
+echo "define('MAILGUN_PUBLIC_KEY','$MAILGUN_PUBLIC_KEY');"
 
