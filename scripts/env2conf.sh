0b2bcb1d88a8a025e77b1cc2e913abda531c4185
Shadi Akiki <shadiakiki1986@gmail.com>
2015-05-18 21:06:26 +0300

    still getting tls ... trying us-east-1 instead of us-west-2

--- scripts/env2conf.sh
+++ scripts/env2conf.sh
@@ -7,12 +7,13 @@
 
 if [ -z "$ZBOOTA_SERVER_AWS_KEY" ]; then echo "Please set AWS_KEY env var"; exit -1; fi
 if [ -z "$ZBOOTA_SERVER_AWS_ACCESS" ]; then echo "Please set ACCESS env vars 1st"; exit -1; fi
+if [ -z "$ZBOOTA_SERVER_AWS_REGION" ]; then echo "Please set Region env vars 1st"; exit -1; fi
 
 echo "<?php"
 echo "define('ROOT','/home/travis/build/shadiakiki1986/zboota-server');"
 echo "define('AWS_PHAR','/home/travis/build/shadiakiki1986/aws.phar');"
 echo "define('AWS_KEY','$ZBOOTA_SERVER_AWS_KEY');"
 echo "define('AWS_SECRET','$ZBOOTA_SERVER_AWS_ACCESS');"
-echo "define('AWS_REGION','us-west-2');"
+echo "define('AWS_REGION','$ZBOOTA_SERVER_AWS_REGION');"
 echo "define('S3_BUCKET','zboota-server');"
 echo "define('S3_FOLDER','photos');"
