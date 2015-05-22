0aab549b3c955d3259051b9c3ca29fd522c0485c
Shadi Akiki <shadiakiki1986@gmail.com>
2015-05-22 14:34:53 +0300

    added test user create api hook

---
+++ var/www/api/testUserCreate.php
@@ -0,0 +1,20 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+/*
+  The test account is the account that I use to test zboota-app (note the app, not the server)
+  It's shadiakiki1986@hotmail.com
+  This api hook just drops that entry so that I can re-create it from the app, testing if that works or not
+*/
+
+require_once dirname(__FILE__).'/../../../config.php';
+require_once ROOT.'/lib/ZbootaTestUser.php';
+
+$ztu=new ZbootaTestUser();
+if($ztu->exists()) {
+	echo "Existing";
+} else {
+	$ztu->create();
+	echo "Created";
+}
