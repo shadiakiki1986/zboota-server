34dbbd39ab3296d1ae54e597f9048f15b8dab8a7
Shadi Akiki <shadiakiki1986@gmail.com>
2015-06-24 06:44:24 +0000

    seems that the sendNotifications has settled with the pml not available issue from last weekend. Setting simulation to false

--- scripts/sendNotifications.php
+++ scripts/sendNotifications.php
@@ -7,5 +7,5 @@ $ddb=connectDynamoDb();
 $zn=new ZbootaNotifications($ddb);
 $zn->deleteNoMoreNotices($zn->getPastMinusCurrent());
 $notices=$zn->getCurrentMinusPast();
-if($zn->sendEmail($notices,true)) $zn->markAsSent($notices); // 2015-06-23: marking as simulated just to watch how the pml=Not available eases out. This should be reverted to false (default) later
+if($zn->sendEmail($notices)) $zn->markAsSent($notices);
 
