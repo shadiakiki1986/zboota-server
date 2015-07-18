bfe7298f28d8e680d55f16a7619f8f4b17776f82
Shadi Akiki <shadiakiki1986@gmail.com>
2015-07-18 07:29:12 +0000

    explicitly added variable SIMULATE to make it easier later if needed [ci skip]

--- scripts/sendNotifications.php
+++ scripts/sendNotifications.php
@@ -3,9 +3,15 @@
 require_once dirname(__FILE__).'/../config.php';
 require_once ROOT.'/lib/ZbootaNotifications.php';
 
+define("SIMULATE",false);
+
 $ddb=connectDynamoDb();
 $zn=new ZbootaNotifications($ddb);
 $zn->deleteNoMoreNotices($zn->getPastMinusCurrent());
 $notices=$zn->getCurrentMinusPast();
-if($zn->sendEmail($notices,false,10)) $zn->markAsSent($notices);
+if($zn->sendEmail($notices,SIMULATE,10)) {
+  if(!SIMULATE) {
+    $zn->markAsSent($notices);
+  }
+}
 
