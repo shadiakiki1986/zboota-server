6b453cb6f34ccb3d40d4d2c989f4c9e826f3a0b4
Shadi Akiki <shadiakiki1986@gmail.com>
2015-07-18 07:27:19 +0000

    replaced tabs with double spaces

--- scripts/dropUnconfirmed.php
+++ scripts/dropUnconfirmed.php
@@ -11,24 +11,24 @@ $unconfirmedSince=date("Y-m-d h:m",strtotime('-30 days'));
 
 // list unconfirmed accounts
 $iterator=$ddb->getIterator('Scan',array(
-    'TableName' => 'zboota-users',
-    'ProjectionExpression' => 'email,lastloginDate,registrationDate',
-    'ExpressionAttributeValues' =>  array (
-	':val1' => array('S' => $unconfirmedSince),
-	':val2' => array('S' => "-")
-    ),
-    'FilterExpression' => 'registrationDate < :val1 AND lastloginDate = :val2' // :val2 = :val2 and :val1=:val1' //
+  'TableName' => 'zboota-users',
+  'ProjectionExpression' => 'email,lastloginDate,registrationDate',
+  'ExpressionAttributeValues' =>  array (
+    ':val1' => array('S' => $unconfirmedSince),
+    ':val2' => array('S' => "-")
+  ),
+  'FilterExpression' => 'registrationDate < :val1 AND lastloginDate = :val2' // :val2 = :val2 and :val1=:val1' //
 ));
 
 // delete
 foreach ($iterator as $item) {
     echo date("Y-m-d H:i")." : Deleting ".$item['email']['S']." from zboota-users because it's an uncofirmed account since {$unconfirmedSince}\n";
-    $ddb->deleteItem(array(
-	'TableName' => 'zboota-users',
-	'Key' => array(
-	    'email'   => array('S' => $item['email']['S'])
-	)
-    ));
+  $ddb->deleteItem(array(
+    'TableName' => 'zboota-users',
+    'Key' => array(
+      'email'   => array('S' => $item['email']['S'])
+    )
+  ));
 }
 
 
